1f99c39f13e1919adbe7b5158942ca48b87ab97f
jainpriya <priya.mca16.du@gmail.com>
2019-02-25 17:51:00 +0530

    check6

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -29,7 +29,7 @@ class UrlsController < ApplicationController
 		    	format.html {render :show}
 		    	format.json { render json: {"response": @url.short_url} }
 		    end
-		end
+		
 	
 
 	def show
